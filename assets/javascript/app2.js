a20920a415e401b5d776fabc9608a940ba0c1307
William Campbell Crocker <williamcrocker1992@gmail.com>
2017-12-02 15:59:28 -0700

    lookingBetter

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,28 +1,138 @@
-//variables
-var showquestions;
-var counter = 30;
-var questionArray = ["In what classic Game did Leeroy Jenkins mess up?", "In what Raid did Leeroy charge in?", "What class was Leeroy Jenkins?", "What was Leeroy's famous last words?"];
-var answerArray = [["World of Warcraft", "Runescape", "Eve Online", "Adventure Quest"], ["Twilights Hammer","BWD","IceCrown Citadel","Neltharians Liar"], ["Warrior", "Rogue", "Paladin", "Death Knight"], ["My loot lol","Godamnit Leeroy","At Least I have Chicken","Sorry Guys"]];
-var Answers = ["A. World of Warcraft", "B. BWD", "C. Paladin", "C. At Least I have Chicken"];
-var selectedAnswer;
-var Clock;
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
+// have timer on page
+//have a start button
+//when the start button is pushed hidden text appears
+
+
+var timer = 30;
+var intervalId;
 
 
 
-$("body").on("click", ".area", function(event){
-	showquestions();
-});
+$("#timer").on("click", run)
 
-function showquestions(){
 
-	for (i = 0; i < questionArray.length; i++) { 
-    console.log(questionArray[i]); }
-$( ".area2" ).text(questionArray);
 
+function run(){
+  intervalId = setInterval(decrement, 1000);
+  
 };
+
+
+function decrement(){
+  timer--;
+  if (timer === 0) {
+    clearInterval(intervalId);
+    alert("PaThEtIc")
+  }
+    $("#timer").html("<h1>" + timer + "</h2>");
+};
+
+
+$("#timer").on("click", show)
+function show(){
+  $("#text").show();
+};
+
+$("#timer").on("click", show2)
+function show2(){
+  $("#submit").show();
+}
+
+
+//-------------------------------------------------------------------------------------------------------------
+// make it so at the end of the timer it counts how many you got right and wrong
+$("#submit").on("click", score)
+
+var correct = 0
+var incorrect = 0
+
+
+
+function score() {
+  questioncheckOne();
+  questioncheckTwo();
+  questioncheckThree();
+  questioncheckFour();
+  console.log(correct);
+  console.log(incorrect);
+  $("#correct1").html(correct)
+  $("#incorrect1").html(incorrect)
+};
+
+function questioncheckOne(){
+    var correctAnswer = document.getElementById("a1")
+    if (correctAnswer.checked == true) {
+        correct++;
+            }
+    else {
+            incorrect ++;
+    }
+    // console.log(correct)
+    // console.log(incorrect)
+};
+
+
+function questioncheckTwo(){
+    var correctAnswer = document.getElementById("b4")
+    if (correctAnswer.checked == true) {
+        correct++;
+    }
+    else {
+            incorrect++;
+    }
+};
+
+
+function questioncheckThree(){
+    var correctAnswer = document.getElementById("c3")
+    if (correctAnswer.checked == true) {
+        correct++;
+    }
+    else {
+            incorrect++;
+    }
+};
+
+
+function questioncheckFour(){
+    var correctAnswer = document.getElementById("d2")
+    if (correctAnswer.checked == true) {
+        correct++;
+        
+    }
+    else {
+            incorrect++;
+    }
+
+ };
+
+$("#submit").on("click", hide)
+
+function hide(){
+  $("#submit").hide();
+};
+
+$("#submit").on("click", leeroy)
+function leeroy(){
+  $("#leeroy").show();
+};
+
+$("#submit").on("click", duh)
+function duh(){
+  $("#reset").show();
+}
+ var reset = document.getElementById('reset');
+
+    reset.addEventListener('click', function(event) {
+      location.reload();
+    }, false);
+
+
+// make it so the text is hidden, the start button is hidden
+// just the score is visible
+// add a restard button that sends it back to the begginning
+//make it so the reset button is hidden until u push submit
+
 
 
 
